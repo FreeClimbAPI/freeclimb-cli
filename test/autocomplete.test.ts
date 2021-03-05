c7aab9c25802c5deee1be68524aa192274aac745
Joseph Black <jblack@vailsys.com>
2021-03-05 14:56:57 -0600

    Fix test skip syntax

--- test/autocomplete.test.ts
+++ test/autocomplete.test.ts
@@ -1,13 +1,14 @@
 import { expect, test } from "@oclif/test"
 
-const testType = process.env.RUNNER_OS === "Windows" ? it.skip : it
-
 describe("Autocomplete", () => {
     test.stdout()
         .command(["autocomplete"])
-        .testType("displays the custom autocomplete message", (ctx) =>
+        .it("displays the custom autocomplete message", function (ctx) {
+            if (process.env.RUNNER_OS === "Windows") {
+                this.skip()
+            }
             expect(ctx.stdout).to.contain(
                 "Copy the following line and run it in your terminal. It will set the FreeClimb CLI autocomplete to load on shell startup."
             )
-        )
+        })
 })
