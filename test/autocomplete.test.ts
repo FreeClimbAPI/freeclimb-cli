6cf20b1831f4081f87697338e6355528057ae366
Joseph Black <jblack@vailsys.com>
2021-03-05 15:18:50 -0600

    Skip at the describe block level

--- test/autocomplete.test.ts
+++ test/autocomplete.test.ts
@@ -1,12 +1,11 @@
 import { expect, test } from "@oclif/test"
 
-describe("Autocomplete", function () {
+const describeType = process.env.RUNNER_OS === "Windows" ? describe.skip : describe
+
+describeType("Autocomplete", () => {
     test.stdout()
         .command(["autocomplete"])
-        .it("displays the custom autocomplete message", function (ctx) {
-            if (process.env.RUNNER_OS === "Windows") {
-                return this.skip()
-            }
+        .it("displays the custom autocomplete message", (ctx) => {
             expect(ctx.stdout).to.contain(
                 "Copy the following line and run it in your terminal. It will set the FreeClimb CLI autocomplete to load on shell startup."
             )
