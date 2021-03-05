974d84ce47e3ac7341067d8d4dbb8cc91aadfbbd
Joseph Black <jblack@vailsys.com>
2021-03-05 15:10:23 -0600

    Remove arrow function that was hiding this

--- test/autocomplete.test.ts
+++ test/autocomplete.test.ts
@@ -1,11 +1,11 @@
 import { expect, test } from "@oclif/test"
 
-describe("Autocomplete", () => {
+describe("Autocomplete", function () {
     test.stdout()
         .command(["autocomplete"])
         .it("displays the custom autocomplete message", function (ctx) {
             if (process.env.RUNNER_OS === "Windows") {
-                this.skip()
+                return this.skip()
             }
             expect(ctx.stdout).to.contain(
                 "Copy the following line and run it in your terminal. It will set the FreeClimb CLI autocomplete to load on shell startup."
