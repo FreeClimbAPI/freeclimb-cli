ae4ddc913032d86af4472217fe14c721416abcf3
Joseph Black <jblack@vailsys.com>
2021-03-05 14:51:41 -0600

    Skip running autocomplete test on Windows platform

--- test/autocomplete.test.ts
+++ test/autocomplete.test.ts
@@ -1,9 +1,11 @@
 import { expect, test } from "@oclif/test"
 
+const testType = process.env.RUNNER_OS === "Windows" ? it.skip : it
+
 describe("Autocomplete", () => {
     test.stdout()
         .command(["autocomplete"])
-        .it("displays the custom autocomplete message", (ctx) =>
+        .testType("displays the custom autocomplete message", (ctx) =>
             expect(ctx.stdout).to.contain(
                 "Copy the following line and run it in your terminal. It will set the FreeClimb CLI autocomplete to load on shell startup."
             )
