b19be22dc0bb25fcdb9be3a8e46e4cbac0d9f8d1
jblack-vail <74741599+jblack-vail@users.noreply.github.com>
2021-04-06 15:23:16 -0500

    Skip running autocomplete test on Windows platform (#29)
    * Skip running autocomplete test on Windows platform
    
    * Fix test skip syntax
    
    * Remove arrow function that was hiding this
    
    * Skip at the describe block level

--- test/autocomplete.test.ts
+++ test/autocomplete.test.ts
@@ -1,11 +1,13 @@
 import { expect, test } from "@oclif/test"
 
-describe("Autocomplete", () => {
+const describeType = process.env.RUNNER_OS === "Windows" ? describe.skip : describe
+
+describeType("Autocomplete", () => {
     test.stdout()
         .command(["autocomplete"])
-        .it("displays the custom autocomplete message", (ctx) =>
+        .it("displays the custom autocomplete message", (ctx) => {
             expect(ctx.stdout).to.contain(
                 "Copy the following line and run it in your terminal. It will set the FreeClimb CLI autocomplete to load on shell startup."
             )
-        )
+        })
 })
