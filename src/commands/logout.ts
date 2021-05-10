4a4233755e168e5756f50a9c78b0142b6bee21ae
Raghu Radhakrishnan <rradhakris@vailsys.com>
2021-05-10 11:03:57 -0400

    Remove a few extra instances of auth token

--- src/commands/logout.ts
+++ src/commands/logout.ts
@@ -3,7 +3,7 @@ import chalk from "chalk"
 import { cred } from "../credentials"
 
 export class logout extends Command {
-    static description = `Remove your saved FreeClimb Account ID and Auth Token from this computer's keychain. This will not remove them from environment variables or config files that you have manually edited.`
+    static description = `Remove your saved FreeClimb Account ID and API Key from this computer's keychain. This will not remove them from environment variables or config files that you have manually edited.`
 
     static flags = {
         help: flags.help({ char: "h" }),
@@ -14,9 +14,7 @@ export class logout extends Command {
         // this does not remove anything that may be set in .env, just what is in keychain
         await cred.removeCredentials(-1)
         this.log(
-            chalk.green(
-                "Successfully removed the saved Account ID and Auth Token from this computer"
-            )
+            chalk.green("Successfully removed the saved Account ID and API Key from this computer")
         )
     }
 }
